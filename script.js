b72a54c77ccedd67914589dccd39fa4901ca616d
Alberto Berenguer Pastor <alberto.berenguer.97@gmail.com>
2018-11-24 20:42:10 +0100

    mas cosas

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function siguiente(){
 function juego(){
   window.location.href="./juego.html";
 }
+function start(){
+  var n = 0;
+  var minutos=0;
+  var l = document.getElementById("number");
+
+  window.setInterval(function(){
+    if(minutos<10){
+      if(n<10)
+        l.innerHTML = "Tiempo: 0"+minutos+":0"+n;
+      else
+          l.innerHTML = "Tiempo: 0"+minutos+":"+n;
+    }
+    else{
+      if(n<10)
+        l.innerHTML = "Tiempo: "+minutos+":0"+n;
+        else
+          l.innerHTML = "Tiempo: " +minutos+":"+n;
+
+    }
+    n++;
+    if(n>60){
+      n=0;
+      minutos++;
+
+    }
+  },1000);
+
+}
